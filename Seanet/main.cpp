b83f922ba52ae4e1aecb8d5946f2d38cbda61d18
Stiven Deleur <deleurapps@gmail.com>
2016-05-12 19:44:38 -0700

    Initial Commit

---
+++ Seanet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Seanet
+//
+//  Created by Stiven Deleur on 5/12/16.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}

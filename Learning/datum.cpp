ac3877ed3e9bb18cf8a3827d3db935052166794a
natcor <douglascorley@douglass-macbook-air.local>
2016-06-01 17:44:49 -0700

    Data class

---
+++ Learning/datum.cpp
@@ -0,0 +1,9 @@
+//
+//  datum.cpp
+//  Seanet
+//
+//  Created by Douglas Corley on 6/1/16.
+//
+//
+
+#include "datum.hpp"
---
+++ Learning/datum.hpp
@@ -0,0 +1,18 @@
+//
+//  datum.hpp
+//  Seanet
+//
+//  Created by Douglas Corley on 6/1/16.
+//
+//
+
+#ifndef datum_hpp
+#define datum_hpp
+
+#include "board.hpp"
+#include "search.hpp"
+#include <stdio.h>
+
+bool isPseudoQuiet(State &state);
+
+#endif /* datum_hpp */

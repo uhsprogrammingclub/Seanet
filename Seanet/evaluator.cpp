83be4063a5bc6e6e126eab4c2edde546d9584ad1
natcor <douglascorley@douglass-macbook-air.local>
2016-06-03 11:11:33 -0700

    PS Evaluations for NN

--- Seanet/evaluator.cpp
+++ Seanet/evaluator.cpp
@@ -235,7 +235,70 @@ int countKingPawnShields(State &state) {
 }
 
 FList getInitialFeatures(State &state) {
-  FList features;
+  FList features(NUM_OF_BOARD_FEATURES);
+  int friendlyPieces[16];
+  int enemyPieces[16];
+  U64 friendlyBB =
+      state._pieceBitboards[state._sideToMove == WHITE ? WHITES : BLACKS];
+  U64 enemyBB =
+      state._pieceBitboards[state._sideToMove == WHITE ? BLACKS : WHITES];
+
+  getSetBits(friendlyBB, friendlyPieces);
+  getSetBits(enemyBB, enemyPieces);
+
+  int i = 0;
+  while (friendlyPieces[i] != -1) {
+    int pcIndex = friendlyPieces[i];
+    Piece piece = state._pieces[pcIndex];
+
+    switch (piece) {
+    case wP:
+      features[FRIENDLY_PAWN_PS] += whitePawnPS[pcIndex];
+      break;
+    case bP:
+      features[FRIENDLY_PAWN_PS] += whitePawnPS[63 - pcIndex];
+      break;
+    case wN:
+      features[FRIENDLY_KNIGHT_PS] += whiteKnightPS[pcIndex];
+      break;
+    case bN:
+      features[FRIENDLY_KNIGHT_PS] += whiteKnightPS[63 - pcIndex];
+      break;
+    case wB:
+      features[FRIENDLY_BISHOP_PS] += whiteBishopPS[pcIndex];
+      break;
+    case bB:
+      features[FRIENDLY_BISHOP_PS] += whiteBishopPS[63 - pcIndex];
+      break;
+    case wR:
+      features[FRIENDLY_ROOK_PS] += whiteRookPS[pcIndex];
+      break;
+    case bR:
+      features[FRIENDLY_ROOK_PS] += whiteRookPS[63 - pcIndex];
+      break;
+    case wQ:
+      features[FRIENDLY_QUEEN_PS] += whiteQueenPS[pcIndex];
+      break;
+    case bQ:
+      features[FRIENDLY_QUEEN_PS] += whiteQueenPS[63 - pcIndex];
+      break;
+    case wK:
+      features[FRIENDLY_KING_PS] +=
+          ((whiteKingMiddlePS[pcIndex] * (256 - state._phase)) +
+           (whiteKingEndPS[pcIndex] * state._phase)) /
+          256;
+      break;
+    case bK:
+      features[FRIENDLY_KING_PS] +=
+          ((whiteKingMiddlePS[63 - pcIndex] * (256 - state._phase)) +
+           (whiteKingEndPS[63 - pcIndex] * state._phase)) /
+          256;
+      break;
+    default:
+      break;
+    }
+  }
+
   return features;
 }
 
